6e30368d6c1207bf3580440956242a9c17c53e09
Darío Blanco <dario.blanco@edelight.de>
2013-01-18 10:10:48 +0100

    Add authentication parameter to the requester script

--- creeper/requester.py
+++ creeper/requester.py
@@ -20,22 +20,33 @@ def parse_arguments():
     """Gets the console arguments"""
     parser = argparse.ArgumentParser(
         description='Creates random requests to a target url')
-    parser.add_argument(
-        "url", type=valid_url, help=("Target url"))
+    parser.add_argument("url", type=valid_url, help=("Target url"))
+    parser.add_argument("-a", "--auth", type=valid_auth, default=None,
+                        help="Auth credentials in 'user:password' format")
     return vars(parser.parse_args())
 
 
 def valid_url(url):
-    """Checks that the given networks are defined in settings"""
+    """Checks that the given url has the proper format"""
     if not regex.match(url):
         raise argparse.ArgumentTypeError(
             "'{0}' is not a valid url".format(url))
     return url
 
 
+def valid_auth(auth):
+    """Checks that the given auth has the proper format"""
+    try:
+        user, password = auth.split(":")
+    except:
+        raise argparse.ArgumentTypeError(
+            "'{0}' is not a valid auth".format(auth))
+    return user, password
+
+
 if __name__ == "__main__":
     arg_dict = parse_arguments()
     ghost = Ghost()
     print "Requesting '{0}'".format(arg_dict['url'])
-    page, resources = ghost.open(arg_dict['url'])
+    page, resources = ghost.open(arg_dict['url'], auth=arg_dict['auth'])
     print ghost.content
